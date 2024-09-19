bf3d2cb5cc945b665ecc73d53e1b53aa1c6c9182
she110ff <youngsoo.j@gmail.com>
2024-09-19 16:03:57 +0900

    mp init

--- index.js
+++
@@ -1,6 +0,0 @@
-import { registerRootComponent } from 'expo';
-import TrackPlayer from 'react-native-track-player';
-import App from './App';
-
-registerRootComponent(App);
-TrackPlayer.registerPlaybackService(() => require('./service'));
